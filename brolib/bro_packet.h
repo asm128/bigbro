3db56d2690cf6b84331cc8d6a51d3e951e85d394
asm128 <pabloarielz@outlook.com>
2019-07-15 01:08:36 -0300

    + Added preliminary code to improve request transmission.

--- brolib/bro_packet.h
+++ brolib/bro_packet.h
@@ -1,4 +1,5 @@
 #include "gpk_http.h"
+#include "gpk_array.h"
 
 #ifndef BRO_PACKET_H_230498209837
 #define BRO_PACKET_H_230498209837
@@ -6,9 +7,14 @@
 namespace bro
 {
 	struct SRequestHeader {
-		::gpk::HTTP_METHOD						Method	;
-		::gpk::view_const_string				Path	;
+		::gpk::HTTP_METHOD						Method		;
+		::gpk::view_const_string				Path		;
+		::gpk::view_const_string				QueryString	;
+		::gpk::view_const_string				ContentBody	;
 	};
+
+	::gpk::error_t			requestWrite	(SRequestHeader & headerToRead, ::gpk::array_pod<byte_t>		& output);
+	::gpk::error_t			requestRead		(SRequestHeader & headerToRead, const ::gpk::view_const_char	& input	);
 } // namespace
 
 #endif // BRO_PACKET_H_230498209837
